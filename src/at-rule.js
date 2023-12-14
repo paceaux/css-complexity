ef2243a1257439eee1c80e6434b700e5b1d5c50a
Frank M. Taylor <frtaylor@redhat.com>
2023-12-14 11:24:16 -0600

    sets up sanitization of text

--- src/at-rule.js
+++ src/at-rule.js
@@ -1,6 +1,10 @@
 class AtRule {
 	static ruleTypeRegex = /@\w+/;
 
+	/**
+	 * @param  {string} atRule=''
+	 * @param  {object} dependencies={}
+	 */
 	constructor(atRule = '', dependencies = {}) {
 		this.dependencies = dependencies;
 
@@ -9,14 +13,46 @@ class AtRule {
 		}
 	}
 
+	/**
+	 * @param  {string} atRuleString
+	 * @returns {string|undefined} the type of at-rule (whatever it starts with, but with @ removed)
+	 */
 	static getAtRuleType(atRuleString) {
 		if (!atRuleString) return undefined;
 		const atRule = atRuleString.match(AtRule.ruleTypeRegex)[0];
 		return atRule.replace('@', '');
 	}
 
+	/**
+	 * @param  {string} atRuleString
+	 * @returns {string|undefined} the condition of the at-rule
+	 */
+	static getConditionText(atRuleString) {
+		if (!atRuleString) return undefined;
+		const atRuleType = AtRule.getAtRuleType(atRuleString);
+		const condition = atRuleString.split(atRuleType)[1].trim();
+		return condition;
+	}
+
+	static sanitizeAtRule(condition) {
+		if (typeof condition !== 'string') throw new Error('condition must be a string');
+
+		const sanitizedCondition = condition
+			.replace(/\s{2,}/g, ' ') // remove double spaces or more
+			.replace('( ', '(')
+			.replace(' )', ')')
+			.replace(/\s?:\s?/g, ':') // remove spaces around :
+			.trim();
+
+		return sanitizedCondition;
+	}
+
 	get atRuleType() {
 		return AtRule.getAtRuleType(this.atRule);
+	}
+
+	get conditionText() {
+		return AtRule.getConditionText(this.atRule);
 	}
 }
 
--- test/at-rule.test.js
+++ test/at-rule.test.js
@@ -4,12 +4,48 @@ import assert from 'node:assert';
 import AtRule from '../src/at-rule.js';
 
 describe('at-rule tests', () => {
-	it('can return the at-rule type from static method', () => {
-		const atRuleType = AtRule.getAtRuleType('@media screen and (min-width: 900px)');
-		assert.equal(atRuleType, 'media');
+	describe('it sanitizes at rules from a static method', () => {
+		it('removes space after a :', () => {
+			const condition = AtRule.sanitizeAtRule('screen and (min-width: 900px)');
+			assert.equal(condition, 'screen and (min-width:900px)');
+		});
+		it('removes spaces around a :', () => {
+			const condition = AtRule.sanitizeAtRule('screen and (min-width : 900px)');
+			assert.equal(condition, 'screen and (min-width:900px)');
+		});
+		it('removes spaces after a (', () => {
+			const condition = AtRule.sanitizeAtRule('screen and ( min-width: 900px)');
+			assert.equal(condition, 'screen and (min-width:900px)');
+		});
+		it('removes spaces before a )', () => {
+			const condition = AtRule.sanitizeAtRule('screen and ( min-width: 900px )');
+			assert.equal(condition, 'screen and (min-width:900px)');
+		});
+		it('removes double spaces)', () => {
+			const condition = AtRule.sanitizeAtRule(' screen and  ( min-width: 900px ) ');
+			assert.equal(condition, 'screen and (min-width:900px)');
+		});
+		it('removes triple   and more         spaces', () => {
+			const condition = AtRule.sanitizeAtRule('@media   screen and       ( min-width  : 900px) ');
+			assert.equal(condition, '@media screen and (min-width:900px)');
+		});
 	});
-	it('can return the at-rule type from getter', () => {
-		const atRule = new AtRule('@media screen and (min-width: 900px)');
-		assert.equal(atRule.atRuleType, 'media');
-	});
+	describe('basic parsing', () => {
+		it('can return the at-rule type from static method', () => {
+			const atRuleType = AtRule.getAtRuleType('@media screen and (min-width: 900px)');
+			assert.equal(atRuleType, 'media');
+		});
+		it('can return the condition from static method', () => {
+			const condition = AtRule.getConditionText('@media screen and (min-width: 900px)');
+			assert.equal(condition, 'screen and (min-width: 900px)');
+		});
+		it('can return the at-rule type from getter', () => {
+			const atRule = new AtRule('@media screen and (min-width: 900px)');
+			assert.equal(atRule.atRuleType, 'media');
+		});
+		it('can return the condition from getter', () => {
+			const atRule = new AtRule('@media screen and (min-width: 900px)');
+			assert.equal(atRule.conditionText, 'screen and (min-width: 900px)');
+		});
+	})
 });
